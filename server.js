582175aa0329d8e7ec9b1269bce0a8c193deafff
yashgujarthi <yashgujarathi25@gmail.com>
2022-12-28 16:43:26 +0530

    adding server

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+
+app.get("/",function(req,resp){
+    resp.send("<h1>Hello World</h1>")
+});
+
+app.listen(3000);
+console.log("Server running on port 3000 ");
